8f5689334ace157fcff6a802c9c3eb083bf85ea9
matteovecchio04 <matteovex@gmail.com>
2025-11-17 12:58:19 +0100

    add Chi Siamo section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Navbar from "./Components/Navbar"
 import Home from "./Components/Home"
+import ChiSiamo from "./Components/ChiSiamo"
 
 export default function App() {
 
@@ -11,6 +12,7 @@ export default function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/chi-siamo" element={<ChiSiamo />} />
           </Routes>
         </div>
       </BrowserRouter>
---
+++ src/Components/ChiSiamo.jsx
@@ -0,0 +1,8 @@
+export default function ChiSiamo() {
+    return (
+        <div className="text-center">
+            <h1>Chi siamo ?</h1>
+            <p>Siamo un e-commerce che vende prodotti falsi e che ama truffarti</p>
+        </div>
+    )
+}
