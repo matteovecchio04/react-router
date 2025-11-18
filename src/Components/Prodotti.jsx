52c32e5ab363b8e34de710c4d6b316829c02aa64
matteovecchio04 <matteovex@gmail.com>
2025-11-18 17:29:04 +0100

    create map but it still doesn't cycle and create the cards

--- src/Components/Prodotti.jsx
+++ src/Components/Prodotti.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react"
+
 export default function Prodotti() {
+    const [prodotti, setProdotti] = useState([])
+
     return (
         <div>
-            <h1>prova</h1>
+            <h1>Prodotti</h1>
+            <div className="row g-3">
+                {prodotti.map((prodotto) => (
+                    <div key={prodotto.id}>
+                        <div className="card">
+                            <img className="card-top" src={prodotto.image} alt="..." />
+                            <div className="card-body">
+                                <h4 className="card-title">{prodotto.title}</h4>
+                                <p className="card-text">${prodotto.price}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
